0b381c1197254bfd6c63953c5b0b3f7cdf7c08d0
Preet Shah <preet.shah1642@gmail.com>
2021-11-04 01:23:33 +0530

    patch_1

--- scf/grep_cutoff.sh
+++ scf/grep_cutoff.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 for dir in */; do
-cd $dir
-echo $dir
+  cd $dir
+  echo $dir
 
-E=$(grep TOTEN OUTCAR|tail -1)
+  E=$(grep TOTEN OUTCAR|tail -1)
 
-#E1=`echo ${E##* }`
-#remove everything except the value
-E1=`echo $E|cut -d ' ' -f5`
+  #E1=`echo ${E##* }`
+  #remove everything except the value
+  E1=`echo $E|cut -d ' ' -f5`
 
-#ZPE="grep zero_point_energy: thermal properties.yanllawk '{print $2]** 9
-#ZPE = grep 'zero_point_energy: thermal properties.yanl
-
-cd ../
-echo ${dir/\//} $E1 | awk '{ print substr( $0, 5) }' >> E.dat
+  cd ../
+  echo ${dir/\//} $E1 | awk '{ print substr( $0, 5) }' >> cutoff.dat
 done
