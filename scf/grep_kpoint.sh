21b6173866b6e564d9d2bc059966e64c9a290eb6
Preet Shah <preet.shah1642@gmail.com>
2021-11-04 01:25:14 +0530

    patch_1

--- scf/grep_kpoint.sh
+++ scf/grep_kpoint.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 for dir in */; do
-cd $dir
-echo $dir
+  cd $dir
+  echo $dir
 
-K=$(grep TOTEN OUTCAR|tail -1)
+  K=$(grep TOTEN OUTCAR|tail -1)
 
-#E1=`echo ${E##* }`
-#remove everything except the value
-K1=`echo $K|cut -d ' ' -f5`
+  #E1=`echo ${E##* }`
+  #remove everything except the value
+  K1=`echo $K|cut -d ' ' -f5`
 
-#ZPE="grep zero_point_energy: thermal properties.yanllawk '{print $2]** 9
-#ZPE = grep 'zero_point_energy: thermal properties.yanl
-
-cd ../
-echo ${dir/\//} $K1 | awk '{ print substr( $0, 5) }' >> K.dat
+  cd ../
+  echo ${dir/\//} $K1 | awk '{ print substr( $0, 5) }' >> cutoff.dat
 done
